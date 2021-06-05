20006342087f720577b204a27e94b1783d1345ae
rohitk7065 <rohit19@navgurukul.org>
2021-06-05 10:23:06 +0530

    ...

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-const express = require("express");
-const connectDB = require("./config/db");
-const cors = require('cors');
-const path = require('path');
-const app = express();
+const express = require("express")
+const connectDB = require("./config/db")
+const cors = require('cors')
+const path = require('path')
+const app = express()
 
 
 // Connect Database
-connectDB();
+connectDB()
 
 // Init middleware
 app.use(cors())
-app.use(express.json({ extended: false }));
+app.use(express.json({ extended: false }))
 
 // Define Routes
-app.use("/api/users", require("./routes/api/users"));
-app.use("/api/profile", require("./routes/api/profile"));
-app.use("/api/posts", require("./routes/api/posts"));
-app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/users", require("./routes/api/users"))
+app.use("/api/profile", require("./routes/api/profile"))
+app.use("/api/posts", require("./routes/api/posts"))
+app.use("/api/auth", require("./routes/api/auth"))
 
 // Serve static assets in production
 if(process.env.NODE_ENV === 'production'){
     // Set static folder
-    app.use(express.static('my-app/build'));
+    app.use(express.static('my-app/build'))
 
     app.get("*",(req,res)=>{
         res.sendFile(path.resolve(__dirname, 'my-app', 'build', 'index.html'))
@@ -31,6 +31,6 @@ if(process.env.NODE_ENV === 'production'){
 
 
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 6000
 
-app.listen(PORT, () => console.log(`server started on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`server started on PORT ${PORT}`))
